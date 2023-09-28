a18d895ca93055d43c3f56a9c8c737b49a12570c
José Javier Rodríguez Gallego <jjavierrg@gmail.com>
2023-09-28 22:32:03 +0200

    Solución 1 del reto

--- Reto/Program.cs
+++ Reto/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reto.Solucion1;
+using System;
 
 namespace Reto
 {
@@ -6,7 +7,23 @@ namespace Reto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            printSolution1();
+        }
+
+        static void printSolution1()
+        {
+            ICorreoElectronico mail = new CorreoElectronico();
+            mail.SetEmisor("jjavierrg@gmail.com");
+            mail.SetReceptor("hello@verramobility.com");
+
+            IContenidoMail contenido1 = new ContenidoSinLetrasA("Texto de prueba. Esta cadena no debería contener letras a");
+            mail.SetContenido(contenido1);
+            Console.WriteLine($"El contenido 1 del mensaje se establece como: {contenido1.GetContenidoCodificado()}");
+
+            IContenidoMail contenido2 = new ContenidoConAdjunto("Fichero de prueba");
+            Console.WriteLine($"El contenido 2 del mensaje se establece como: {contenido2.GetContenidoCodificado()}");
+            mail.SetContenido(contenido2);
+
         }
     }
 }
--- Reto/Soluciones/Solucion1.cs
+++ Reto/Soluciones/Solucion1.cs
@@ -2,5 +2,49 @@
 
 namespace Reto.Solucion1
 {
-    
+    public interface IContenidoMail
+    {
+        string GetContenidoCodificado();
+    }
+
+    public interface ICorreoElectronico
+    {
+        void SetEmisor(string emisor);
+        void SetReceptor(string receptor);
+        void SetContenido(IContenidoMail contenido);
+    }
+
+    public class ContenidoSinLetrasA: IContenidoMail
+    {
+        private readonly string _contenido;
+
+        public ContenidoSinLetrasA(string contenido) => _contenido = contenido;
+
+        public string GetContenidoCodificado() => _contenido?.Replace("a", "");
+    }
+
+    public class ContenidoConAdjunto : IContenidoMail
+    {
+        private readonly string _nombreAdjunto;
+
+        public ContenidoConAdjunto(string nombreAdjunto) => _nombreAdjunto = nombreAdjunto;
+
+        public string GetContenidoCodificado() => $"Adjuntado fichero con nombre {_nombreAdjunto}";
+    }
+
+    public class CorreoElectronico : ICorreoElectronico
+    {
+        public void SetEmisor(string emisor)
+        {
+
+        }
+        public void SetReceptor(string receptor)
+        {
+
+        }
+        public void SetContenido(IContenidoMail contenido)
+        {
+
+        }
+    }
 }
